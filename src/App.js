6ca4624717e5d2157749910b86de5e7dca585d28
elexier <pazamoryperdon@gmail.com>
2021-07-26 19:14:24 -0500

    Aplicacion de useReducer a crudApi

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Contador from './components/Contador';
 import ContadorMejorado from './components/ContadorMejorado';
 import ShoppingCart from './components/ShoppingCart';
+import CrudApi from './components/CrudApi';
 
 function App() {
   return (
     <div className="App">
       <h1>Use Reducer</h1>
+      <CrudApi/>
       <ShoppingCart/>
       <hr/>
       <ContadorMejorado/>
---
+++ src/components/Message.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Message = ({msg, bgColor}) => {
+
+    let styles = {
+        padding: "1rem",
+        marginBottom:"1rem",
+        textAlign:"center",
+        backgroundColor: bgColor,
+        color:"#fff",
+        fontWeight:"bold"
+    }
+
+    return (
+        <div style={styles}>           
+            {/* <p>{msg}</p> */}
+            <p dangerouslySetInnerHTML={{__html:msg}}></p>
+        </div>
+    )
+}
+
+export default Message
